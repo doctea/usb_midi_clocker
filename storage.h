74dfa1578da2147fe08d4193d731dbd67bc9b981
doctea <doctea@gmail.com>
2022-04-03 12:31:46 +0100

    untested versioning of save states (previous version didnt include clock delay setting)

--- storage.h
+++ storage.h
@@ -4,11 +4,14 @@
 #define NUM_SEQUENCES 4
 #define NUM_STEPS     8
 
+#define SAVE_ID_BYTE_V0 0xD0
+#define SAVE_ID_BYTE_V1 0xD1
+
 typedef struct savestate {
-  byte id = 0xD0;
-  byte size_clocks = NUM_CLOCKS;
+  byte id = SAVE_ID_BYTE_V1;
+  byte size_clocks    = NUM_CLOCKS;
   byte size_sequences = NUM_SEQUENCES;
-  byte size_steps = NUM_STEPS;
+  byte size_steps     = NUM_STEPS;
   byte clock_multiplier[NUM_CLOCKS] = { 5, 4, 3, 2 };
   byte sequence_data[NUM_SEQUENCES][NUM_STEPS];
   byte clock_delay[NUM_CLOCKS] = { 0, 0, 0, 0 };
@@ -26,18 +29,23 @@ void save_state(byte preset_number, savestate *input) {
 void load_state(byte preset_number, savestate *output) {
   int eeAddress = 16 + (preset_number * sizeof(savestate));
   byte id = EEPROM.read(eeAddress);
-  if (id!=0xD0) {
-    Serial.print(F("Didn't find magic id "));
-    Serial.print(0xD0);
-    Serial.print(F(" at "));
+  if (id==SAVE_ID_BYTE_V0 || id==SAVE_ID_BYTE_V1) {
+    Serial.print(F("Found ID "));
+    Serial.print(id);
+    Serial.print(" at ");
+    Serial.print(eeAddress);
+    Serial.println(F(" - loading! :D"));
+    EEPROM.get(eeAddress, *output);
+    if (id==SAVE_ID_BYTE_V0) {  // v0 didn't have clock delays included, so zero them out
+      output->clock_delay[0] = output->clock_delay[1] = output->clock_delay[2] = output->clock_delay[3] = 0;
+    }
+  } else {
+    Serial.print(F("Didn't find a magic id byte at "));
+    //Serial.print(0xD0);
+    //Serial.print(F(" at "));
     Serial.print(eeAddress);
     Serial.print(F(" - found "));
     Serial.print(id);
     Serial.println(F(" instead :("));
-  } else {
-    Serial.print(F("Found ID at "));
-    Serial.print(eeAddress);
-    Serial.println(F(" - loading!"));
-    EEPROM.get(eeAddress, *output);
   }    
 }
