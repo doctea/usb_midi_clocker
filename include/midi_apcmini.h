c5a48ea12a2e36bd5fd038babc19870cf0e4aab4
doctea <doctea@gmail.com>
2022-08-29 20:31:25 +0100

    rename file to main.cpp, so that platform.io won't feel the need to create a boilerplate main.cpp (in case #1 is a problem for others)

--- include/midi_apcmini.h
+++ include/midi_apcmini.h
@@ -38,23 +38,6 @@
 #define BUTTON_RESTART_IMMEDIATELY    APCMINI_BUTTON_UP
 #define BUTTON_RESTART_AT_END_OF_BAR  APCMINI_BUTTON_DEVICE
 
-
-
-//MIDI_NAMESPACE::MidiInterface<UHS2MIDI_NAMESPACE::uhs2MidiTransport> *midi_apcmini;
-//extern MIDIDevice_BigBuffer *midi_apcmini;
-//extern volatile uint8_t ixAPCmini;  //= 0xff;
-
-//extern volatile bool apcmini_started;// = false;
-//extern bool apcmini_shift_held;// = false;
-/*
-#ifdef ENABLE_CLOCKS
-    extern byte clock_selected;// = 0;
-#endif
-
-extern bool redraw_immediately;// = false;
-extern unsigned long last_updated_display;// = 0;
-*/
-
 #ifdef ENABLE_APCMINI_DISPLAY
     void apcmini_update_clock_display();
     void apcmini_update_position_display(int ticks);
@@ -70,14 +53,5 @@ void apcmini_loop(unsigned long ticks);
 void apcmini_note_on(uint8_t inChannel, uint8_t inNumber, uint8_t inVelocity);
 void apcmini_note_off(uint8_t inChannel, uint8_t inNumber, uint8_t inVelocity);
 void apcmini_control_change(uint8_t inChannel, uint8_t inNumber, uint8_t inValue);
-
-/*
-// called inside interrupt
-void apcmini_on_tick(volatile uint32_t ticks);
-// called inside interrupt
-void apcmini_on_restart();
-void apcmini_init();
-*/
-//#endif
 
 #endif
