3b4cdf6d42180dee4ade5089f9716230999661b3
doctea <doctea@gmail.com>
2022-09-16 21:30:14 +0100

    almost compiles, but i think i need to refactor the parameters library further

--- include/parameters/MIDICCParameter.h
+++ include/parameters/MIDICCParameter.h
@@ -18,13 +18,14 @@ class MIDICCParameter : public Parameter<DeviceBehaviourUltimateBase,byte> {
                 //this->cc_setter = sendControlChange;
                 this->cc_number = cc_number;
                 this->channel = channel;
+                this->minimumDataValue = 0;
+                this->maximumDataValue = 127;
         }
 
         MIDICCParameter(char* label, DeviceBehaviourUltimateBase *target, byte cc_number, byte channel, byte maximum_value) 
             : MIDICCParameter(label, target, cc_number, channel) {
-                this->maximum_value = maximum_value;
+                //this->maximumNormalValue = maximum_value;
         }
-
 
         /*virtual const char* parseFormattedDataType(byte value) {
             static char fmt[20] = "              ";
@@ -36,34 +37,45 @@ class MIDICCParameter : public Parameter<DeviceBehaviourUltimateBase,byte> {
         virtual const char* getFormattedValue() override {
             //return this->parseFormattedDataType(this->get_midi_value_for_double(this->getCurrentValue()));
             static char fmt[20] = "              ";
-            sprintf(fmt, "%i    ", this->get_midi_value_for_double(this->getCurrentValue()));
+            //sprintf(fmt, "%i", this->get_midi_value_for_double(this->getCurrentNormalValue()));
+            sprintf(fmt, "%i", this->getCurrentDataValue()); //get_midi_value_for_double(this->getCurrentNormalValue()));
             //Serial.printf("getFormattedValue: '%s'\n", fmt);
             return fmt;
         };
 
-        virtual byte get_midi_value_for_double(double value) {
+        /*virtual const char* getFormattedValue(double value) override {
+            //return this->parseFormattedDataType(this->get_midi_value_for_double(this->getCurrentValue()));
+            static char fmt[20] = "              ";
+            sprintf(fmt, "%i    ", this->get_midi_value_for_double(this->getCurrentValue()));
+            //Serial.printf("getFormattedValue: '%s'\n", fmt);
+            return fmt;
+        };*/
+
+        // takes a -0.5f to +0.5f value and converts it into a midi 0-127 value
+        /*virtual byte get_midi_value_for_double(double value) {
             if (this->debug) Serial.printf("MIDICCParameter#get_midi_value_for_double passed %f\n", value);
             //value = (value + 1.0f) / 2.0;
             value = value + 0.5f;
             value = constrain(value, 0.0, 1.0);
             if (this->debug) Serial.printf("MIDICCParameter#get_midi_value_for_double re-normalised to %f\n", value);
-            byte bvalue = value * this->maximum_value;  //127
+            byte bvalue = value * this->maximumNormalValue;  //127
             return bvalue;
-        }
+        }*/
 
-        virtual void actual_set_value(double value) override {
-            /*if (this->debug) Serial.printf("MIDICCParameter#actual_set_value passed %f\n", value);
+        virtual void setTargetValueFromData(byte value) override {
+            /*if (this->debug) Serial.printf("MIDICCParameter#setTargetValueFromData passed %f\n", value);
             value = (value + 1.0f) / 2.0;
-            if (this->debug) Serial.printf("MIDICCParameter#actual_set_value re-normalised to %f\n", value);
+            if (this->debug) Serial.printf("MIDICCParameter#setTargetValueFromData re-normalised to %f\n", value);
             byte bvalue = value * 127.0;*/
             //this->debug = true;
-            byte bvalue = this->get_midi_value_for_double(value);
+            //byte bvalue = this->get_midi_value_for_double(value);
+            //byte bvalue = this->getCurrentDataValue();
             
             if (this->target!=nullptr) {
-                if (this->debug) Serial.printf("MIDICCParameter#actual_set_value(%i, %i, %i)\n", cc_number, bvalue, this->channel);
-                this->target->sendControlChange(this->cc_number, (byte)bvalue, this->channel);
+                if (this->debug) Serial.printf("MIDICCParameter#setTargetValueFromData(%i, %i, %i)\n", cc_number, value, this->channel);
+                this->target->sendControlChange(this->cc_number, (byte)value, this->channel);
             } else {
-                if (this->debug) Serial.printf("WARNING: No target set in MIDICCParameter#actual_set_value in '%s'!\n", this->label);
+                if (this->debug) Serial.printf("WARNING: No target set in MIDICCParameter#setTargetValueFromData in '%s'!\n", this->label);
             }
         }
 
