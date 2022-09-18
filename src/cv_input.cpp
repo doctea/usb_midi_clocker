fad3c8d84a35b86dc654f7a50117f721641b9dfa
doctea <doctea@gmail.com>
2022-09-18 14:15:10 +0100

    tidy up old code from cv input setup

--- src/cv_input.cpp
+++ src/cv_input.cpp
@@ -22,174 +22,70 @@ ADS24vVoltageSource<ADS1015> voltage_source_1_channel_1 = ADS24vVoltageSource<AD
 
 ParameterManager parameter_manager = ParameterManager();
 
+// initialise the voltage-reading hardware/librareis and the ParameterManager
 void setup_cv_input() {
     Serial.println((char*)"setup_cv_input...");
     tft_print((char*)"...setup_cv_input...\n");
 
-    /*//Serial.println("Beginning ADS_x48..");
-    Serial.println("Beginning ADS_24V..");
-    ADS_OBJECT_24V.begin();
-    ADS_OBJECT_24V.setGain(2);
-    voltage_sources.add(&voltage_source_1_channel_0);
-    voltage_sources.add(&voltage_source_1_channel_1);
-    //voltage_sources.add(&voltage_source_1_channel_2);
-    voltage_source_1_channel_0.correction_value_1 = 1180.0;    */
-    //parameter_manager.debug = true;
     parameter_manager.init();
-    parameter_manager.addADCDevice(new ADCPimoroni24v(ENABLE_CV_INPUT, 5.0)); //, 5.0)); //, 2, MAX_INPUT_VOLTAGE_24V));
+
+    #ifdef ENABLE_CV_INPUT
+        parameter_manager.addADCDevice(new ADCPimoroni24v(ENABLE_CV_INPUT, 5.0)); //, 5.0)); //, 2, MAX_INPUT_VOLTAGE_24V));
+    #endif
 
     parameter_manager.auto_init_devices();
 
     tft_print((char*)"..done setup_cv_input\n");
 }
 
-/*
-// ParameterInputs, ie wrappers around input mechanism, assignable to a Parameter
-LinkedList<BaseParameterInput*> available_inputs            = LinkedList<BaseParameterInput*>();
-
-// Parameters, ie wrappers around destination object
-LinkedList<DataParameter*>      available_parameters    = LinkedList<DataParameter*>();
-
-//DataParameter* parameter_a = nullptr;
-DataParameter  param_none                              = DataParameter((char*)"None");
-*/
-
+// initialise the input voltage ParameterInputs that can be mapped to Parameters
 void setup_parameters() {
     // add the available parameters to a list used globally and later passed to each selector menuitem
     Serial.println(F("==== begin setup_parameters ===="));
     tft_print((char*)"setup_parameters...");
 
-    // TODO: ask the behaviour to automatically configure its known parameters
-    // add them to sub-menus
-
-    /*MIDICCParameter *parameter_a = new MIDICCParameter(
-        (char*)"CS Cutoff", 
-        //(MIDIOutputWrapper*)midi_matrix_manager->get_target_for_handle((char*)"USB : CraftSynth : ch 1"), 
-        behaviour_craftsynth,
-        (byte)34,
-        (byte)1
-    );    // cutoff
-    MIDICCParameter *parameter_b = new MIDICCParameter(
-        (char*)"CS Spread", 
-        //(MIDIOutputWrapper*)midi_matrix_manager->get_target_for_handle((char*)"USB : CraftSynth : ch 1"), 
-        behaviour_craftsynth,
-        (byte)20,
-        (byte)1
-    );    // spread
-
-    parameter_manager.addParameter(parameter_a);
-    parameter_manager.addParameter(parameter_b);
-
+    // initialise the voltage source inputs
     // todo: improve this bit, maybe name the voltage sources?
-    */
     VoltageParameterInput<BaseParameter> *vpi1 = new VoltageParameterInput<BaseParameter>('A', parameter_manager.voltage_sources.get(0));
     VoltageParameterInput<BaseParameter> *vpi2 = new VoltageParameterInput<BaseParameter>('B', parameter_manager.voltage_sources.get(1));
     VoltageParameterInput<BaseParameter> *vpi3 = new VoltageParameterInput<BaseParameter>('C', parameter_manager.voltage_sources.get(2));
-    //vpi1->map_unipolar_to_bipolar = true;
-    //vpi2->map_unipolar_to_bipolar = true;
-    //vpi1->debug = true;
+
+    // tell the parameter manager about them
     parameter_manager.addInput(vpi1);
     parameter_manager.addInput(vpi2);
     parameter_manager.addInput(vpi3);
 
-    /*vpi1->setTarget(parameter_a);
-    vpi2->setTarget(parameter_b);*/
-
+    // get the available target parameters
+    // todo: dynamically pull these from all available behaviours
+    // todo: dynamically pull them from other things that could have parameters available
     LinkedList<DoubleParameter*> *params = behaviour_craftsynth->get_parameters();
     parameter_manager.addParameters(params);
 
+    // setup the default mappings
+    // TODO: make this a property of the Parameter, not the ParameterInputs, so we can have multiple mappings
+    // TODO: load this from a saved config file
     Serial.println("=========== SETTING DEFAULT PARAMETER MAPS.........");
-    vpi1->setTarget(behaviour_craftsynth->getParameterForLabel("Cutoff")); //params->get(0));
-    vpi2->setTarget(behaviour_craftsynth->getParameterForLabel("Filter Morph")); //params->get(1));
+    vpi1->setTarget(behaviour_craftsynth->getParameterForLabel("Cutoff")); 
+    vpi2->setTarget(behaviour_craftsynth->getParameterForLabel("Filter Morph"));
     vpi3->setTarget(behaviour_craftsynth->getParameterForLabel("Distortion")); 
     Serial.println("=========== FINISHED SETTING DEFAULT PARAMETER MAPS");
-    //vpi2->setTarget(params->get(3));
-
-    /*VoltageParameterInput<BaseParameter> *vpi1 = new VoltageParameterInput<BaseParameter>('A', voltage_sources.get(0));
-    available_inputs.add(vpi1);
-    vpi1->setTarget(parameter_a);
-
-    VoltageParameterInput<BaseParameter> *vpi2 = new VoltageParameterInput<BaseParameter>('B', voltage_sources.get(1));
-    available_inputs.add(vpi2);
-    vpi2->setTarget(parameter_b);
-
-    //vpi1->debug = parameter_a->debug = true;
-
-    //VoltageParameterInput<BaseParameter> *vpi3 = new VoltageParameterInput<BaseParameter>('C', voltage_sources.get(2));
-    //available_inputs.add(vpi3);
-
-    available_parameters.add(&param_none);
-    available_parameters.add(parameter_a);
-    available_parameters.add(parameter_b);
-
-    //vpi2->setTarget(parameter_b);*/
 
     tft_print((char*)"\n");
 }
-    
+
+// set up the menus to provide control over the Parameters and ParameterInputs
 void setup_parameter_menu() {
     Serial.println("==== setup_parameter_menu starting ====");
-    //menu->add(&PulseWidthModulationPanel);
-    //menu->add(&testitem);
 
     Serial.println("Adding ParameterSelectorControls for available_inputs...");
+    // ask ParameterManager to add all the menu items for the ParameterInputs
     parameter_manager.addAllParameterInputMenuItems(menu);
-    //parameter_manager.addAllParameterMenuItems(menu);
 
+    // ask ParameterManager to add all the menu items for the Parameters
+    // todo: dynamically loop over all the available behaviours
     parameter_manager.addParameterSubMenuItems(menu, behaviour_craftsynth->get_label(), behaviour_craftsynth->get_parameters());
-
-    /*for (int i = 0 ; i < available_inputs.size() ; i++) {
-        BaseParameterInput *param_input = available_inputs.get(i);
-        BaseParameter *param = param_input->target_parameter;
-
-        Serial.printf("About to rename param_input %i '%c'..\n", i, param_input->name);
-
-        param_input->name = i+'A';
-
-        char input_label[20];
-        //sprintf(input_label, "Input %i => %c", i<4?0:1, param_input->name); //i+'A');
-        sprintf(input_label, "Input %c", param_input->name); //i+'A');
-
-        Serial.printf("%i: Creating control labelled '%s'...\n", i, input_label); Serial.flush();
-        ParameterSelectorControl *ctrl = new ParameterSelectorControl(input_label);
-
-        if (param==nullptr) {
-            Serial.printf("\t%i: Configuring it with available_inputs item target (no target_parameter already set)..\n", i); Serial.flush();    
-        } else {
-            Serial.printf("\t%i: Configuring it with available_inputs item target '%s'..\n", i, param->label); Serial.flush();
-        }
-        //BaseParameterInput *param_input = available_inputs.get(i);
-        ctrl->configure(param_input, &available_parameters);
-
-        Serial.println("\tConfigured!  Adding to menu..");
-        //ctrl->debug = true;
-        menu->add(ctrl);
-        Serial.println("\tAdded!"); Serial.flush();
-    }*/
-
-    /*
-    Serial.println("Starting add available_parameters loop..."); Serial.flush();
-    Serial.printf("Got %i parameters..\n", available_parameters.size());
-    for (int i = 0 ; i < available_parameters.size() ; i++) {
-        DataParameter *p = available_parameters.get(i);
-        if (strcmp(p->label,"None")==0) 
-            continue;
-        Serial.printf("\tinstantiating ParameterMenuItem for item %i aka %s\n", i, p->label); Serial.flush();
-        MenuItem *ctrl = p->makeControl();
-        Serial.printf("\tadding it to menu...\n", i, p->label); Serial.flush();
-        menu->add(ctrl);
-    }
-    Serial.println("Finished adding available_parameters loop"); Serial.flush();
-    */
 
     Serial.println("setup_parameter_menu done ==================");
 }
 
 
-// called every loop to update the inputs; maybe in future we also eventually want to process internal LFOs or other properties of parameter here
-/*void update_parameters() {
-    for (int i = 0 ; i < available_inputs.size() ; i++) {
-        available_inputs.get(i)->loop();
-        //parameter_inputs[i]->loop();
-    }
-}*/
