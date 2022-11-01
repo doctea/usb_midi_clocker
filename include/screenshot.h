954326bbab9c08ac55dee110c206562070ecde2c
Tristan <tristan@3dtotal.com>
2022-11-01 12:06:40 +0000

    just some silly little changes to screenshot function

--- include/screenshot.h
+++ include/screenshot.h
@@ -29,14 +29,14 @@ bool save_screenshot(ST7789_t3 *screen) {
 
     Serial.println(F("save_screenshot!"));
 
-    char name[] = "9px_0000.bmp";       // filename convention (will auto-increment)
+    char name[] = "umc_0000.bmp";       // filename convention (will auto-increment)
     const int w = screen->width();                   // image width in pixels
     const int h = screen->height();                    // " height
     //const boolean debugPrint = true;    // print details of process over serial?
         
     const int imgSize = w*h;
     //int px[w*h];                        // actual pixel data (grayscale - added programatically below)
-    uint16_t *px = screen->getFrameBuffer();
+    const uint16_t *px = screen->getFrameBuffer();
 
     SdFile file;
 
@@ -54,15 +54,15 @@ bool save_screenshot(ST7789_t3 *screen) {
     Serial.printf(F("Found one in %s!\n"), name); Serial.flush();
 
     // set fileSize (used in bmp header)
-    int rowSize = 4 * ((3*w + 3)/4);      // how many bytes in the row (used to create padding)
-    int fileSize = 54 + h*rowSize;        // headers (54 bytes) + pixel data
+    const int rowSize = 4 * ((3*w + 3)/4);      // how many bytes in the row (used to create padding)
+    const int fileSize = 54 + h*rowSize;        // headers (54 bytes) + pixel data
 
     // create image data; heavily modified version via:
     // http://stackoverflow.com/a/2654860
-    unsigned char *img = NULL;            // image data
+    /*unsigned char *img = NULL;            // image data
     if (img) {                            // if there's already data in the array, clear it
         free(img);
-    }
+    }*/
     //img = (unsigned char *)malloc(3*imgSize);
 
     // create padding (based on the number of pixels in a row
@@ -98,11 +98,11 @@ bool save_screenshot(ST7789_t3 *screen) {
     for (int y=0; y<h; y++) {                            // iterate image array
         //file.write(img+(w*(h-i-1)*3), 3*w);                // write px data
         for (int x = 0 ; x < w ; x++) {
-            int addr = imgSize - ((y * w) + (w-x));
-            uint16_t v = px[addr];
-            unsigned char red = (v & 0xf800) >> 11;
-            unsigned char green = (v & 0x07e0) >> 5;
-            unsigned char blue = v & 0x001f;
+            const int addr = imgSize - ((y * w) + (w-x));
+            const uint16_t v = px[addr];
+            const unsigned char red = (v & 0xf800) >> 11;
+            const unsigned char green = (v & 0x07e0) >> 5;
+            const unsigned char blue = v & 0x001f;
             //file.write(&px[i], 2*w);
             file.write(blue << 3);
             file.write(green << 2);
@@ -116,8 +116,6 @@ bool save_screenshot(ST7789_t3 *screen) {
         Serial.print("\n\n---\n");
     //}
     
-
     return true;
-
 }
 
