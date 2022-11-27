d19496b9d4f2b87e05232471c87a30b8c1b3828e
doctea <doctea@gmail.com>
2022-11-27 14:32:15 +0000

    checking Serial before writing

--- include/debug.h
+++ include/debug.h
@@ -8,16 +8,16 @@ void reset_teensy();
 
 #ifndef Serial_flush
     #ifdef SERIAL_FLUSH_REALLY
-        #define Serial_flush() Serial.flush()
+        #define Serial_flush() if(Serial)Serial.flush()
     #else
         #define Serial_flush() {}
     #endif
 #endif
 #ifndef Debug_printf
     #ifdef ENABLE_DEBUG_SERIAL
-        #define Debug_println(X)    Serial.println(X)
-        #define Debug_printf(...)     Serial.printf(__VA_ARGS__)
-        #define Debug_print(X)      Serial.print(X)
+        #define Debug_println(X)    if(Serial)Serial.println(X)
+        #define Debug_printf(...)   if(Serial)Serial.printf(__VA_ARGS__)
+        #define Debug_print(X)      if(Serial)Serial.print(X)
     #else
         #define Debug_println(X)    {}
         #define Debug_printf(...)     {}
