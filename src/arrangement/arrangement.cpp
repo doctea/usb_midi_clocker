447480a1cc01c94da5484f5e6042b6ed5f3cc53c
Tristan <tristan@3dtotal.com>
2022-11-28 15:11:13 +0000

    untested refactor to have ArrangementTracks manage constituent clips; assumes stacking multiple clips might be an option for some types of track..

--- src/arrangement/arrangement.cpp
+++ src/arrangement/arrangement.cpp
@@ -4,10 +4,18 @@ ClipManager *clip_manager = nullptr;
 
 void setup_clip_manager() {
     clip_manager = new ClipManager();
+
+    // for initial testing -- add a clip for every sequence slot
     for (int i = 0 ; i < NUM_SEQUENCE_SLOTS_PER_PROJECT ; i++) {
         clip_manager->add_clip(new SequenceClip(i));
     }
+
+    for (int i = 0 ; i < NUM_LOOP_SLOTS_PER_PROJECT ; i++) {
+        clip_manager->add_clip(new LoopClip(i));
+    }
+
 }
+
 
 #include "arrangement/arrangement.h"
 
@@ -16,8 +24,17 @@ Arrangement *arrangement = nullptr;
 void setup_arrangement() {
     Serial.println("setup_arrangement...");
     arrangement = new Arrangement();
-	for (int i = 0 ; i < 8 ; i++) {
-        Serial.printf("setup_arrangement %i\n", i);
-		arrangement->insert_clip_instance(i, clip_manager->get_clip_for_id(7-i));
+    ArrangementTrackBase *sequencer_track = arrangement->addTrack(new ArrangementSingleTrack("Sequence"));
+	for (int i = 0 ; i < NUM_SEQUENCE_SLOTS_PER_PROJECT ; i++) {
+        Serial.printf("setup_arrangement for a sequence %i\n", i);
+		sequencer_track->insert_clip_instance(i, clip_manager->add_clip(new LoopClip(7-i))); //new SequenceClip(7-i)); //clip_manager->get_clip_for_id(7-i));
 	}
+
+    ArrangementTrackBase *loop_arrangement = arrangement->addTrack(new ArrangementSingleTrack("Looper"));
+    for (int i = 0 ; i < NUM_LOOP_SLOTS_PER_PROJECT ; i++) {
+        loop_arrangement->insert_clip_instance(random(0,16), clip_manager->add_clip(new LoopClip(i)));
+    }
+
+    // TODO: loop over each behaviour, add an ArrangementTrack as appropriate...
+    // probably ask the behaviour itself to instantiate the track object
 }
