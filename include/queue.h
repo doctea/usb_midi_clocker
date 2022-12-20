bc0bed0f113a8bdb97ebd1f8a5a2b8c373b03f69
doctea <doctea@gmail.com>
2022-12-20 16:28:06 +0000

    commented queue, removed compile warning

--- include/queue.h
+++ include/queue.h
@@ -18,21 +18,26 @@ class Queue {
 
     Queue() {};
 
+    // add an item to queue, with optional timeout and delay
     void push(DataType data, uint32_t timeout = 0, uint32_t delay = 0) {
-        tail = ++tail % max_queue_size;
+        tail++;
+        tail %= max_queue_size;
         memcpy(&queue[tail], &data, sizeof(DataType));
 
         this->delay_at[tail] = millis() + delay;    // record the time that we want to request this
         this->timeout[tail] = timeout;              // record the timeout to allow before pushing on
         //Serial.printf("pushed to tail %i\n", tail);
     }
+    // dequeue first queued item, returning a pointer to the popped item or nullptr if queue is empty
     DataType *pop() {
         if (this->isEmpty()) // nothing to do 
             return nullptr;
-        head = ++head % max_queue_size;
+        head++;
+        head %= max_queue_size;
         memcpy(&head_data, &queue[head], sizeof(DataType));
         //Serial.printf("popped from head %i: %02x,%02x\n", head, head_data.pp, head_data.cc);
         this->actual_timeout_at = millis() + timeout[head]; // record time that item was popped, so we can timeout if still paused when that happens
+        // todo: set the paused flag here based on whether a timeout was requested, rather than requiring the calling code to handle this?
 
         return &head_data;
     }
@@ -41,6 +46,7 @@ class Queue {
         return head==tail;
     }
 
+    // is ready to deliver a value (ie queue not empty and not waiting on a delay; also checks+clears timeout flag if necessary)
     bool isReady() {
         if (this->isEmpty())            
             // not ready if nothing queued!
@@ -56,6 +62,7 @@ class Queue {
         return !paused;
         //return this->paused || this->isEmpty();
     }
+    // set the pause override flag
     void setPaused(bool state) {
         this->paused = state;
     }
