4aed3e806a8cff72c1793c2952ebdf8db4c0ed39
doctea <doctea@gmail.com>
2023-01-29 00:21:58 +0000

    backport ForceOctave SaveableParameterClass and remove()->unlink() from saveable_parameter_class

--- include/behaviours/behaviour_midibass.h
+++ include/behaviours/behaviour_midibass.h
@@ -141,8 +141,11 @@ class MIDIBassBehaviour : virtual public DeviceBehaviourUltimateBase {
         virtual void setup_saveable_parameters() override {
             if (this->saveable_parameters==nullptr)
                 DeviceBehaviourUltimateBase::setup_saveable_parameters();
-            this->saveable_parameters->add(new SaveableParameter<MIDIBassBehaviour, bool>("drone", this, &this->drone_enabled, nullptr, nullptr, &MIDIBassBehaviour::set_drone, &MIDIBassBehaviour::is_drone));
-            this->saveable_parameters->add(new SaveableParameter<MIDIBassBehaviour, int8_t>("machinegun", this, &this->machinegun, nullptr, nullptr, &MIDIBassBehaviour::set_machinegun, &MIDIBassBehaviour::get_machinegun));
+
+            MIDIOutputWrapper *my_wrapper = midi_matrix_manager->get_target_for_id(this->target_id);
+            this->saveable_parameters->add(new SaveableParameter<MIDIOutputWrapper, int>("octave", "MIDI Bass", my_wrapper, nullptr, nullptr, nullptr, &MIDIOutputWrapper::setForceOctave, &MIDIOutputWrapper::getForceOctave));
+            this->saveable_parameters->add(new SaveableParameter<MIDIBassBehaviour, bool>("drone", "MIDI Bass", this, &this->drone_enabled, nullptr, nullptr, &MIDIBassBehaviour::set_drone, &MIDIBassBehaviour::is_drone));
+            this->saveable_parameters->add(new SaveableParameter<MIDIBassBehaviour, int8_t>("machinegun", "MIDI Bass", this, &this->machinegun, nullptr, nullptr, &MIDIBassBehaviour::set_machinegun, &MIDIBassBehaviour::get_machinegun));
         }
 
         /*virtual void save_sequence_add_lines(LinkedList<String> *lines) override {
