47d11e8fb7bf6689026e1482b69fdc102794e672
Tristan <tristan@3dtotal.com>
2023-09-05 12:25:00 +0100

    remove unnecessary reference to wrapper

--- include/behaviours/behaviour_midibass.h
+++ include/behaviours/behaviour_midibass.h
@@ -170,7 +170,7 @@ class MIDIBassBehaviour : virtual public DeviceBehaviourUltimateBase {
             if (this->saveable_parameters==nullptr)
                 DeviceBehaviourUltimateBase::setup_saveable_parameters();
 
-            MIDIOutputWrapper *my_wrapper = midi_matrix_manager->get_target_for_id(this->target_id);
+            //MIDIOutputWrapper *my_wrapper = midi_matrix_manager->get_target_for_id(this->target_id);
             this->saveable_parameters->add(new SaveableParameter<DeviceBehaviourUltimateBase, int>("octave", "MIDI Bass", this, nullptr, nullptr, nullptr, &DeviceBehaviourUltimateBase::setForceOctave, &DeviceBehaviourUltimateBase::getForceOctave));
             this->saveable_parameters->add(new SaveableParameter<MIDIBassBehaviour, bool>("drone", "MIDI Bass", this, &this->drone_enabled, nullptr, nullptr, &MIDIBassBehaviour::set_drone, &MIDIBassBehaviour::is_drone));
             this->saveable_parameters->add(new SaveableParameter<MIDIBassBehaviour, int8_t>("machinegun", "MIDI Bass", this, &this->machinegun, nullptr, nullptr, &MIDIBassBehaviour::set_machinegun, &MIDIBassBehaviour::get_machinegun));
