b9ba97aaa5f1791b4f7ad9c656cb4313247069ac
doctea <doctea@gmail.com>
2023-11-17 22:50:16 +0000

    fix garbage on din midi clock indicator

--- src/behaviours/behaviour_base_serial.cpp
+++ src/behaviours/behaviour_base_serial.cpp
@@ -12,9 +12,9 @@ const char *DeviceBehaviourSerialBase::get_indicator() {
         String s = String(
             (receives_midi_notes() ? String(this->input_midi_number+1) :" ") + " " +
             (transmits_midi_notes()? String(this->output_midi_number+1):" ") + " " + 
-            (transmits_midi_clock()? "C" : " ")
+            String(transmits_midi_clock()? "C" : " ")
         );
-        strncpy(this->indicator_text, s.c_str(), 5);
+        strncpy(this->indicator_text, s.c_str(), 8);
         indicator_done = true;
     }
     return this->indicator_text;
