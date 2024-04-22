a2083b1b5cfbdf591948badb95b21fd2c2f78011
doctea <doctea@gmail.com>
2024-04-22 22:01:19 +0100

    moved creation of cvinput menus to midihelpers library

--- src/behaviours/behaviour_cvinput.cpp
+++ src/behaviours/behaviour_cvinput.cpp
@@ -6,7 +6,6 @@
 #include "bpm.h"
 
 #include "scales.h"
-#include "mymenu/menuitems_scale.h"
 
 extern bool debug_flag;
 
@@ -75,7 +74,9 @@ extern bool debug_flag;
         */
         //Serial.println(F("DeviceBehaviour_CVInput::make_menu_items() setting up ParameterInputSelectorControl")); Serial_flush();
         //ParameterInputSelectorControl<DeviceBehaviour_CVInput> *pitch_parameter_selector 
+
         SubMenuItemBar *bar = new SubMenuItemBar("Inputs");
+
         this->pitch_parameter_selector 
             = new ParameterInputSelectorControl<DeviceBehaviour_CVInput> (
                 "1v/oct Input",
@@ -97,143 +98,10 @@ extern bool debug_flag;
                 this->velocity_input
         );
         bar->add(velocity_parameter_selector);
-        menuitems->add(bar);
-
-        #ifdef DEBUG_VELOCITY
-            DirectNumberControl<int8_t> *velocity_control = new DirectNumberControl<int8_t>("Velocity", &this->velocity, 127, 0, 127);
-            menuitems->add(velocity_control);
-        #endif
-
-        //Serial.println(F("DeviceBehaviour_CVInput::make_menu_items() setting up HarmonyStatus")); Serial_flush();
-        HarmonyStatus *harmony = new HarmonyStatus(
-            "CV->MIDI pitch", 
-            &this->chord_player.last_note, 
-            &this->chord_player.current_note,
-            &this->chord_player.current_raw_note,
-            "Raw"
-        );
-        menuitems->add(harmony);
-
-        bar = new SubMenuItemBar("Trigger/durations");
-        //Serial.println(F("about to create length_ticks_control ObjectSelectorControl..")); Serial_flush();
-        LambdaSelectorControl<int32_t> *length_ticks_control = new LambdaSelectorControl<int32_t>(
-                "Note length",
-                [=](int32_t v) -> void { this->chord_player.set_note_length(v); },
-                [=]() -> int32_t { return this->chord_player.get_note_length(); },
-                nullptr,
-                true
-        );
-        //Serial.println(F("about to add values..")); Serial_flush();
-        length_ticks_control->add_available_value(0,                 "None");
-        length_ticks_control->add_available_value(PPQN/PPQN,         "-");
-        length_ticks_control->add_available_value(PPQN/8,            "32nd");
-        length_ticks_control->add_available_value(PPQN/4,            "16th");
-        length_ticks_control->add_available_value(PPQN/3,            "12th");
-        length_ticks_control->add_available_value(PPQN/2,            "8th");
-        length_ticks_control->add_available_value(PPQN,              "Beat");
-        length_ticks_control->add_available_value(PPQN*2,            "2xBeat");
-        length_ticks_control->add_available_value(PPQN*4,            "Bar");
-        //Serial.println(F("about to add to menuitems list..")); Serial_flush();
-        bar->add(length_ticks_control);
-
-        //Serial.println(F("about to create length_ticks_control ObjectSelectorControl..")); Serial_flush();
-        LambdaSelectorControl<int32_t> *trigger_ticks_control = new LambdaSelectorControl<int32_t>(
-                "Trigger each",
-                [=](int32_t v) -> void { this->chord_player.set_trigger_on_ticks(v); },
-                [=]() -> int32_t { return this->chord_player.get_trigger_on_ticks(); },
-                nullptr,
-                true
-        );
-        trigger_ticks_control->add_available_value(0,                 "Change");
-        //trigger_ticks_control->add_available_value(PPQN/PPQN,         "-");
-        trigger_ticks_control->add_available_value(PPQN/8,            "32nd");
-        trigger_ticks_control->add_available_value(PPQN/4,            "16th");
-        trigger_ticks_control->add_available_value(PPQN/3,            "12th");
-        trigger_ticks_control->add_available_value(PPQN/2,            "8th");
-        trigger_ticks_control->add_available_value(PPQN,              "Beat");
-        trigger_ticks_control->add_available_value(PPQN*2,            "2xBeat");
-        trigger_ticks_control->add_available_value(PPQN*4,            "Bar");
-        //Serial.println(F("about to add to menuitems list..")); Serial_flush();
-        bar->add(trigger_ticks_control);
-
-        LambdaSelectorControl<int32_t> *trigger_delay_ticks_control 
-            = new LambdaSelectorControl<int32_t>(
-                "Delay",
-                [=](int32_t v) -> void { this->chord_player.set_trigger_delay_ticks(v); },
-                [=]() -> int32_t { return this->chord_player.get_trigger_delay_ticks(); },
-                nullptr,
-                true
-        );
-        //trigger_ticks_control->add_available_value(0,                 "Change");
-        //trigger_ticks_control->add_available_value(PPQN/PPQN,         "-");
-        trigger_delay_ticks_control->add_available_value(0,                 "None");
-        trigger_delay_ticks_control->add_available_value(PPQN/8,            "32nd");
-        trigger_delay_ticks_control->add_available_value(PPQN/4,            "16th");
-        trigger_delay_ticks_control->add_available_value(PPQN/3,            "12th");
-        trigger_delay_ticks_control->add_available_value(PPQN/2,            "8th");
-        trigger_delay_ticks_control->add_available_value(PPQN,              "Beat");
-        trigger_delay_ticks_control->add_available_value(PPQN*2,            "2xBeat");
-        //trigger_ticks_control->add_available_value(PPQN*4,            "Bar");
-        bar->add(trigger_delay_ticks_control);
 
         menuitems->add(bar);
 
-        #ifdef CVINPUT_CONFIGURABLE_CHANNEL
-            menuitems->add(new LambdaNumberControl<byte>("Channel", [=](byte v) -> void { this->chord_player.set_channel(v); }, [=]() -> byte { return this->chord_player.get_channel(); }));
-        #endif
-
-        //menuitems->add(new ToggleControl<bool>("Debug", &this->debug));
-
-        // TODO: move scale/key and quantise items to a dedicated class that can be re-used
-        // TODO: make mono, fake-poly, and true-poly versions of class:-
-        //          mono forces one note at a time, and doesn't offer auto-chord function?
-        //          fake-poly offers auto-chord functions
-        //          true-poly doesn't offer auto-chord functions
-        //          all versions offer quantisation to scale
-        // TODO: allow all pitched behaviours to use a 'global scale' setting (-1?)
-        menuitems->add(new LambdaScaleMenuItemBar(
-            "Scale / Key", 
-            [=](SCALE scale) -> void { this->chord_player.set_scale(scale); }, 
-            [=]() -> SCALE { return this->chord_player.get_scale(); },
-            [=](int8_t scale_root) -> void { this->chord_player.set_scale_root(scale_root); },
-            [=]() -> int8_t { return this->chord_player.get_scale_root(); },
-            true
-        ));
-
-        bar = new SubMenuItemBar("Quantise / chords");
-        bar->add(new LambdaToggleControl("Quantise",    
-            [=](bool v) -> void { this->chord_player.set_quantise(v); },
-            [=]() -> bool { return this->chord_player.is_quantise(); }
-        ));
-        bar->add(new LambdaToggleControl("Play chords", 
-            [=](bool v) -> void { this->chord_player.set_play_chords(v); },
-            [=]() -> bool { return this->chord_player.is_play_chords(); }
-        ));
-
-        LambdaSelectorControl<CHORD::Type> *selected_chord_control = new LambdaSelectorControl<CHORD::Type>(
-            "Chord", 
-            [=](CHORD::Type chord_type) -> void { this->chord_player.set_selected_chord(chord_type); }, 
-            [=]() -> CHORD::Type { return this->chord_player.get_selected_chord(); },
-            nullptr, true
-        );
-        for (size_t i = 0 ; i < NUMBER_CHORDS ; i++) {
-            selected_chord_control->add_available_value(i, chords[i].label);
-        }
-
-        bar->add(new LambdaNumberControl<int8_t>(
-            "Inversion", 
-            [=](int8_t v) -> void { this->chord_player.set_inversion(v); }, 
-            [=]() -> int8_t { return this->chord_player.get_inversion(); },
-            nullptr, 0, 4, true
-        ));
-        bar->add(selected_chord_control);
-
-        menuitems->add(bar);
-
-        menuitems->add(new ChordMenuItem("Current chord",   &this->chord_player.current_chord_data));
-        menuitems->add(new ChordMenuItem("Last chord",      &this->chord_player.last_chord_data));
-
-        menuitems->add(new ToggleControl<bool>("Debug", &this->debug));
+        this->chord_player.make_menu_items(menuitems);
 
         //Serial.println(F("returning..")); Serial_flush();
         return menuitems;
