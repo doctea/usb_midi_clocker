06495b40ce55a02fa890ca046af29a7f08e5ec85
doctea <doctea@gmail.com>
2024-09-15 23:37:07 +0100

    bitbox behaviour has 10 mappable CC outputs

--- include/ConfigMidi.h
+++ include/ConfigMidi.h
@@ -21,6 +21,7 @@
 
 #ifdef PCB_GO
     #define ENABLE_BITBOX               MIDI1   // used for output
+    #define ENABLE_BITBOX_DEDICATED
 #endif
 #ifdef PCB_STUDIO
     #define ENABLE_NEUTRON              MIDI3   // bass output on MIDI3   // WTF TODO FIX CRASH - crashes during 'doing addParameters -> behaviour neutron -> ??' when no screen is enabled
