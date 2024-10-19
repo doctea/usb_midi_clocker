d814edd56363b885f49edd10f4e1b64d541a2d7c
doctea <doctea@gmail.com>
2024-10-19 23:32:49 +0100

    3x 'Mixer' parameter inputs for intense meta-modulation

--- src/cv_input.cpp
+++ src/cv_input.cpp
@@ -87,8 +87,12 @@ void setup_parameters() {
     parameter_manager->addInput(virtpi2);
     parameter_manager->addInput(virtpi3);
 
-    VirtualMixerParameterInput *mixerpi1 = new VirtualMixerParameterInput((char*)"Mixer", "Mixers");
+    VirtualMixerParameterInput *mixerpi1 = new VirtualMixerParameterInput((char*)"Mix 1", "Mixers");
+    VirtualMixerParameterInput *mixerpi2 = new VirtualMixerParameterInput((char*)"Mix 2", "Mixers");
+    VirtualMixerParameterInput *mixerpi3 = new VirtualMixerParameterInput((char*)"Mix 3", "Mixers");
     parameter_manager->addInput(mixerpi1);
+    parameter_manager->addInput(mixerpi2);
+    parameter_manager->addInput(mixerpi3);
 
     // get the available target parameters
     // todo: dynamically pull them from other things that could have parameters available
