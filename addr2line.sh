0703d5ce52ba7f4e41c79fe0b4f88def39d5aaae
doctea <doctea@gmail.com>
2024-10-31 21:56:23 +0000

    not sure whats going on here, strange merging behaviour

--- addr2line.sh
+++ addr2line.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-~/.platformio/packages/toolchain-gccarmnoneeabi/bin/arm-none-eabi-addr2line.exe -a $2 -e .pio/build/$1/firmware.elf
+~/.platformio/packages/toolchain-gccarmnoneeabi/bin/arm-none-eabi-addr2line.exe -a $2 -e .pio/build/$1/firmware_$1.elf
