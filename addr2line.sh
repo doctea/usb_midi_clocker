876f8d6cee8da30aa0f6fdf3aff11c8508304010
doctea <doctea@gmail.com>
2025-01-25 17:18:04 +0000

    make addr2line script work (hardcoded my local path tho)

--- addr2line.sh
+++ addr2line.sh
@@ -5,6 +5,8 @@ ADDR=$2
 
 echo "Using firmware file '$FIRMWARE' and address $2.."
 
-~/.platformio/packages/toolchain-gccarmnoneeabi/bin/arm-none-eabi-addr2line.exe -a $2 -e .pio/build/$1/firmware_$1.elf
+#~/.platformio/packages/toolchain-gccarmnoneeabi/bin/arm-none-eabi-addr2line.exe -a $2 -e .pio/build/$1/firmware_$1.elf
+
+/mnt/c/Users/doctea/.platformio/packages/toolchain-gccarmnoneeabi/bin/arm-none-eabi-addr2line.exe -a $2 -e .pio/build/$1/firmware_$1.elf
 
 
