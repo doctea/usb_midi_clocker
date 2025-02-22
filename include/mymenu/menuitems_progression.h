d6e9b57e4fa2ec8ec70c7fec5280c959cfab84c0
doctea <doctea@gmail.com>
2025-02-22 00:24:26 +0000

    WIP UI indication of progression+apc info in pinned menu

--- include/mymenu/menuitems_progression.h
+++ include/mymenu/menuitems_progression.h
@@ -8,42 +8,26 @@
 #include "menuitems.h"
 #include "submenuitem_bar.h"
 
-class ProgressionPinnedMenuItem : public PinnedPanelMenuItem {
+class ProgressionPinnedMenuItem : /*virtual public SubMenuItemBar,*/ virtual public PinnedPanelMenuItem {
     public:
         SubMenuItemBar *progression_bar = nullptr;
-        ProgressionPinnedMenuItem(const char *label) : PinnedPanelMenuItem(label) {
-            this->progression_bar = new SubMenuItemBar(label, true, true);
-
-            this->setup_progression_bar();
-
+        ProgressionPinnedMenuItem(const char *label) : /*SubMenuItemBar(label),*/ PinnedPanelMenuItem(label) {
+            //this->setup_progression_bar();
         };
 
-        void setup_progression_bar();
+        //void setup_progression_bar();
 
-        virtual int display(Coord pos, bool selected, bool opened) override {
-            int y = pos.y;
-            if (this->progression_bar!=nullptr) {
-                y += this->progression_bar->display(Coord(pos.x, y), selected, opened);
-            }
-            return y;
-        }
-
-        virtual void on_add() override {
-            if (this->progression_bar!=nullptr) {
-                this->progression_bar->on_add();
-            }
-        }
+        //using SubMenuItemBar::display;
+        /*virtual int display(Coord pos, bool selected, bool opened) override {
+            return PinnedPanelMenuItem::display(pos, selected, opened);
+        }*/
+        //using PinnedPanelMenuItem::tft;
+        //using SubMenuItemBar::on_add;
 
         /*virtual int display(Coord pos, bool selected, bool opened) override {
-            int y = pos.y;
-            int x = 0;
-            tft->setCursor(0, y);
-            tft->setTextSize(1);
-            tft->setTextColor(C_WHITE, BLACK);
-            // show playlist position
-            tft->printf("[%i/%i]", behaviour_progression->playlist_position, 8);
-            // show current section
-            // show current lock status
-            return y;
+            SubMenuItemBar::tft->println("wtf?");
+            return SubMenuItemBar::tft->getCursorY();
         }*/
+
+        virtual int display(Coord pos, bool selected, bool opened) override;
 };
--- src/mymenu/menuitems_progression.cpp
+++ src/mymenu/menuitems_progression.cpp
@@ -2,14 +2,85 @@
 
 #include "behaviours/behaviour_progression.h"
 
-void ProgressionPinnedMenuItem::setup_progression_bar() {
+#include "midi/midi_apcmini_display.h"
+
+// todo: make a widget that indicates graphically the song structure
+// so, loop over playlist; for each playlist position,
+// draw the section repeat/2 times
+// for each section, draw the chord progression
+// each chord progression section should indicate the chord, inversion, and quality .. somehow
+
+// todo: indicate whether the current section has been saved 
+
+int ProgressionPinnedMenuItem::display(Coord pos, bool selected, bool opened) {
+    int y = pos.y;
+    int x = 0;
+    tft->setCursor(0, y);
+
+    // make bar numbers bigger if we're in a scrollable menu
+    if (menu->get_selected_page()->scrollable) 
+        tft->setTextSize(2);
+    else 
+        tft->setTextSize(1);
+
+    tft->setTextColor(C_WHITE, BLACK);
+    
+    //// headers for the columns
+    tft->printf("Pos Cnt Sec Bar ");
+
+    // indicate what page the APC is on
+    if (get_apc_gate_page()==CLOCKS)
+        tft->printf("Clk");
+    else if (get_apc_gate_page()==SEQUENCES)
+        tft->printf("Seq");
+    else if (get_apc_gate_page()==PATTERNS)
+        tft->printf("Pat");
+    #ifdef ENABLE_APCMINI_PROGRESSIONS
+        else if (get_apc_gate_page()==PROGRESSIONS)
+            tft->printf("Prg");
+    #endif
+    #ifdef ENABLE_ACPMINI_PADS
+        else if (get_apc_gate_page()==PADS)
+            tft->printf("Pad");
+    #endif
+    else
+        tft->printf("???");
+    tft->println();
+    
+    // show playlist information
+    tft->printf("%i/%i ", behaviour_progression->playlist_position+1, 8);
+    tft->printf("%i/%i ", behaviour_progression->current_section_plays+1, behaviour_progression->playlist.entries[behaviour_progression->playlist_position].repeats);
+    tft->printf("%i/%i ", behaviour_progression->current_section+1, 4);
+    tft->printf("%i/%i ", behaviour_progression->current_bar+1, 8);
+
+    // indicate current apc page info
+    if (get_apc_gate_page()==PROGRESSIONS) {
+        tft->println(   
+            behaviour_progression->current_mode==VirtualBehaviour_Progression::MODE::DEGREE ?       "Deg" : 
+            behaviour_progression->current_mode==VirtualBehaviour_Progression::MODE::QUALITY ?      "Qlt" : 
+            behaviour_progression->current_mode==VirtualBehaviour_Progression::MODE::INVERSION ?    "Inv" : 
+            behaviour_progression->current_mode==VirtualBehaviour_Progression::MODE::PLAYLIST ?     "Pls" : "???"
+        );
+    } else {
+        tft->println("---");
+    }
+    // todo: show current lock status
+
+    y = tft->getCursorY() - 4;
+    return y;
+}
+
+
+
+/*void ProgressionPinnedMenuItem::setup_progression_bar() {
     // show playlist position
-    this->progression_bar->add(new NumberControl<int8_t>("PlayPos", &behaviour_progression->playlist_position, 0, 8, true, true));
+    this->add(new NumberControl<int8_t>("PlayPos", &behaviour_progression->playlist_position, 0, 8, true, true));
     // show number of plays of this section
-    this->progression_bar->add(new NumberControl<int8_t>("Plays", &behaviour_progression->current_section_plays, 0, 8, true, true));
+    this->add(new NumberControl<int8_t>("Plays", &behaviour_progression->current_section_plays, 0, 8, true, true));
     // show current section number
-    this->progression_bar->add(new NumberControl<int8_t>("Section", &behaviour_progression->current_section, 0, 3, true, true));
+    this->add(new NumberControl<int8_t>("Section", &behaviour_progression->current_section, 0, 3, true, true));
     // show current bar number
-    this->progression_bar->add(new NumberControl<int8_t>("Bar", &behaviour_progression->current_bar, 0, 7, true, true));
+    this->add(new NumberControl<int8_t>("Bar", &behaviour_progression->current_bar, 0, 7, true, true));
     // show current key, chord, inversion
 }
+*/
