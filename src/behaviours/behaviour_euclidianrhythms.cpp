b7ee29f436131fc23cc51db077542449e90fc571
Tristan Rowley <doctea@gmail.com>
2025-03-30 20:48:54 +0100

    restored euclidian behaviour contents from github copy... (in a bit of a mess trying to disentangle these branches!)

--- src/behaviours/behaviour_euclidianrhythms.cpp
+++ src/behaviours/behaviour_euclidianrhythms.cpp
@@ -2,4 +2,12 @@
 
 #ifdef ENABLE_EUCLIDIAN
     VirtualBehaviour_EuclidianRhythms *behaviour_euclidianrhythms;
+
+    void shuffled_track_callback(uint8_t track, uint32_t step) {
+        //Serial.printf("at tick %i, shuffled_track_callback(%i, %i)\n", ticks, track, step);
+        if (behaviour_euclidianrhythms!=nullptr) {
+                behaviour_euclidianrhythms->on_step_shuffled(track, step);
+        }
+    }
+
 #endif
