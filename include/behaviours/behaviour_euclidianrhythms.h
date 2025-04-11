4a5583ecff92ba498bfce522e74ea1ccdf64bdfe
Tristan Rowley <doctea@gmail.com>
2025-04-12 00:06:10 +0100

    remove shuffle setup that was breaking stuff

--- include/behaviours/behaviour_euclidianrhythms.h
+++ include/behaviours/behaviour_euclidianrhythms.h
@@ -47,12 +47,13 @@ class VirtualBehaviour_EuclidianRhythms : virtual public DeviceBehaviourUltimate
                 (int8_t)0, (int8_t)0, (int8_t)3, (int8_t)0, (int8_t)0, (int8_t)-3, (int8_t)0, (int8_t)0, 
                 (int8_t)0, (int8_t)0, (int8_t)3, (int8_t)0, (int8_t)0, (int8_t)-3, (int8_t)0, (int8_t)0
             };*/
-            int8_t shuff[] = { 
+            /*int8_t shuff[] = { 
                 (int8_t)-2, (int8_t)2, (int8_t)3, (int8_t)3, (int8_t)3, (int8_t)-3, (int8_t)-2, (int8_t)3, 
                 (int8_t)1, (int8_t)3, (int8_t)3, (int8_t)1, (int8_t)-2, (int8_t)-2, (int8_t)-1, (int8_t)0
             };
             uClock.setTrackShuffleTemplate(1, shuff, 16);
             uClock.setTrackShuffle(1, true);
+            */
         #endif
     }
 
